94ec291389bbd27b2258e5095d2e683917002a9e
DanielMilanez <daniel.augusto.milanez@gmail.com>
2025-12-01 19:20:24 -0300

    Refatorando
    Reajustando parametros e desenvolvendo função para teste basico

---
+++ inc/dependences.h
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void Delay(int ms){
+    struct timespec ts;
+    ts.tv_sec = ms / 1000;
+    ts.tv_nsec = (ms % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+}
+
+void TestDelay(void){
+    printf("Contando segundos: \n");
+
+    for(int i = 0; i < 10; ++i){
+        Delay(1000);
+        printf("%d\n", i + 1);
+    }
+}
