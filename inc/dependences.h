747adf344acda99abe483ce9080efc8f4f810760
DanielMilanez <daniel.augusto.milanez@gmail.com>
2025-12-01 21:56:33 -0300

    Desenvolvendo funções de dependecia basicas

--- inc/dependences.h
+++ inc/dependences.h
@@ -1,14 +1,33 @@
-#include <time.h>
+#ifndef DEPENDENCES_H
+#define DEPENDENCES_H
+
 #include <stdio.h>
+#include <windows.h>
+#include <time.h>
+#include <stdint.h>
 #include <conio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <wchar.h>
+#include <wincon.h>
 
-void Delay(int ms){
-    struct timespec ts;
-    ts.tv_sec = ms / 1000;
-    ts.tv_nsec = (ms % 1000) * 1000000;
-    nanosleep(&ts, NULL);
-}
+typedef enum{
+    K_LEFT,
+    K_RIGHT,
+    K_UP,
+    K_DOWN
+} vk_typedef;
 
+#define WIDTH 90
+#define HEIGHT 30
+
+#define true 1
+#define false 0
+
+void Delay(int ms);
+void Hide_Cursor(void);
+uint8_t Keyboard_Check(vk_typedef key);
+void Render_Frame(CHAR_INFO buffer[HEIGHT][WIDTH]);
+void Start_Up(int *x, int *y, CHAR_INFO buffer[HEIGHT][WIDTH]);
+
+#endif
